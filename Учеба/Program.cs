3e50c916b7c464626439030d9ab9551462b9bd1c
XanterWolker <anton.goyman@mail.ru>
2021-02-25 00:43:36 +0700

    Начало массивов

--- Учеба/Program.cs
+++ Учеба/Program.cs
@@ -6,29 +6,8 @@ namespace Учеба
     {
         static void Main(string[] args)
         {
- 
-            double sum = 100 ;
-            double div = sum / 3;
-            Console.WriteLine($"Введите коэфициенты квадратного уравнения ax^2 + bx + c =0");
-
-            Console.Write("a = ");
-            int a = int.Parse(Console.ReadLine());
-
-            Console.Write("b = ");
-            int b = int.Parse(Console.ReadLine());
-
-            Console.Write("c = ");
-            int c = int.Parse(Console.ReadLine());
-
-            double d = b * b - 4 * a * c;
-
-            Console.WriteLine($"Дискриминант = {d}");
-
-            double x1 = (-b - Math.Sqrt(d)) / 2 * a;
-            double x2 = (-b + Math.Sqrt(d)) / 2 * a;
-
-            Console.WriteLine($"Корни уравнения x1 = {x1}, x2 = {x2}");
-
+            int[] oneDimmension = new int[10];
+            double[] doubleArray = { 1.5, 22.11, 9.99 };
         }
     }
 }
