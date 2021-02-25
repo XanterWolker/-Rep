896bcffe472388c921b191922b99e7eb6a0514af
XanterWolker <anton.goyman@mail.ru>
2021-02-25 22:52:52 +0700

    Массив

--- Учеба/Program.cs
+++ Учеба/Program.cs
@@ -6,18 +6,23 @@ namespace Учеба
     {
         static void Main(string[] args)
         {
-           /* int num = 20;
-           num =+ 5;
-            Console.WriteLine( "Переменная:" + num);
-           */
+            /* int num = 20;
+            num =+ 5;
+             Console.WriteLine( "Переменная:" + num);
+            */
 
-           
 
+            /*
             double num = 1.4234;
             double cub = Math.Pow(num, 3);
             cub = Math.Round(cub);
 
             Console.WriteLine(cub);
+            */
+
+            char[] arr = new char[] {'J', 'a', 'v', 'a', '!' };
+
+            Console.WriteLine("Элемент: " + arr[2]);
         }
     }
 }
