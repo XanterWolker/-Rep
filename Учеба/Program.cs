f23afc8b589483fa588ffae64235b7561dad6ec2
XanterWolker <anton.goyman@mail.ru>
2021-02-25 23:11:36 +0700

    1Task

--- Учеба/Program.cs
+++ Учеба/Program.cs
@@ -4,25 +4,113 @@ namespace Учеба
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Найти минимальный элемент массива
+        /// </summary>
+        public static void Task1()
         {
-            /* int num = 20;
-            num =+ 5;
-             Console.WriteLine( "Переменная:" + num);
-            */
+            int[] a = new int[] { 5, 12, 13, 2, 1, 9, 15, 19, 6 };
+
+            int min = 0;
+
+            for (int i = 0; i < a[8]; i++)
+            {
+                if (a[0] < a[1])
+                {
+                    min = a;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Найти два наибольших элемента массива
+        /// </summary>
+        public static void Task2()
+        {
+            int[] a = new int[] { 5, 12, 13, 2, 1, 9, 15, 19, 6 };
+        }
+
+        /// <summary>
+        /// Посчитать сумму элементов массива
+        /// </summary>
+        public static void Task3()
+        {
+            int[] a = new int[] { 5, 12, 13, 2, 1, 9, 15, 19, 6 };
+        }
+
+        /// <summary>
+        /// Заполнить массив по возрастанию от 1 до 100
+        /// </summary>
+        public static void Task4()
+        {
+
+        }
+
+        /// <summary>
+        /// Создать и заполнить массив случайными целыми числами
+        /// </summary>
+        public static void Task5()
+        {
+            //пример вызова генератора случайных чисел, он понадобится в данной задаче
+            Random rnd = new Random();
+            int r = rnd.Next(1, 10);//случайное число от 1 до 10
+        }
+
+        /// <summary>
+        /// Проверить, что в массиве нет одинаковых чисел
+        /// </summary>
+        public static void Task6()
+        {
+            int[] a = new int[] { 5, 12, 13, 2, 1, 9, 15, 19, 6 };
+        }
+
+        /// <summary>
+        /// Переставить элементы массива в обратном порядке используя вспомогательный массив
+        /// </summary>
+        public static void Task7()
+        {
+            int[] a = new int[] { 5, 12, 13, 2, 1, 9, 15, 19, 6 };
+        }
+
+        /// <summary>
+        /// Переставить элементы массива в обратном порядке НЕ используя вспомогательный массив
+        /// </summary>
+        public static void Task8()
+        {
+            int[] a = new int[] { 5, 12, 13, 2, 1, 9, 15, 19, 6 };
+        }
+
+        /// <summary>
+        /// Подсчитать сумму чисел в двухмерном массиве
+        /// </summary>
+        public static void Task9()
+        {
+            int[,] m = new int[,] { { 11, 22, 31 }, { 4, 53, 6 }, { 7, 81, 90 } };
+        }
+
+        /// <summary>
+        /// Заполнить двумерный массив 10 на 10 случайными числами от 1 до 9 и вывести на экран консоли
+        /// </summary>
+        public static void Task10()
+        {
+        }
+
+        /// <summary>
+        /// Двумерный массив скопировать в одномерный
+        /// </summary>
+        public static void Task11()
+        {
+            int[,] m = new int[,] { { 11, 22, 31 }, { 4, 53, 6 }, { 7, 81, 90 } };
+        }
 
 
-            /*
-            double num = 1.4234;
-            double cub = Math.Pow(num, 3);
-            cub = Math.Round(cub);
-
-            Console.WriteLine(cub);
-            */
-
-            char[] arr = new char[] {'J', 'a', 'v', 'a', '!' };
-
-            Console.WriteLine("Элемент: " + arr[2]);
+        /// <summary>
+        /// Заполнить двумерный массив 10 на 10 случайными числами от 1 до 99 и определить количество четных чисел в массиве
+        /// </summary>
+        public static void Task12()
+        {
+            //подсказка: для определения остатка от деления используется оператор %
+            int ostatok = 4 % 2; //будет равен нулю
         }
     }
 }
