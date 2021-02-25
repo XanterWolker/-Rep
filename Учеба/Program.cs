05dc5d0a8e90561426371f5574ba182caae9f2d9
XanterWolker <anton.goyman@mail.ru>
2021-02-25 16:55:56 +0700

    sum

--- Учеба/Program.cs
+++ Учеба/Program.cs
@@ -6,36 +6,13 @@ namespace Учеба
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vvedite n");
-            int n = Convert.ToInt32(Console.ReadLine());
+            int num = 20;
+           num =+ 5;
+            Console.WriteLine( "Переменная:" + num);
 
-            int[,] z = new int[n, n];
 
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    z[i, j] = (i + 1) * (j + 1);
-                }
-            }
+
             
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"\t{i+1}");
-            }
-            Console.WriteLine();
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"{i + 1}");
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write($"\t{z[j,i]}");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-
         }
     }
 }
