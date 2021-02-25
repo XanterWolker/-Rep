2521c51782dac0b1efdb00156bf5c64e7f39462d
XanterWolker <anton.goyman@mail.ru>
2021-02-25 14:01:05 +0700

    Табл умнж

--- Учеба/Program.cs
+++ Учеба/Program.cs
@@ -7,28 +7,28 @@ namespace Учеба
         static void Main(string[] args)
         {
             Console.WriteLine("Vvedite n");
-            int n = Convert.ToInt32(Console.ReadLine);
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            int[,] z = new int[5, 5];
+            int[,] z = new int[n, n];
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < n; j++)
                 {
                     z[i, j] = (i + 1) * (j + 1);
                 }
             }
             
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.Write($"\t{i+1}");
             }
             Console.WriteLine();
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.Write($"{i + 1}");
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < n; j++)
                 {
                     Console.Write($"\t{z[j,i]}");
                 }
